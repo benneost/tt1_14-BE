f59f744f9a53dbf95225bb8d730fff9512bb4612
vanessageena <vanessahu2704@gmail.com>
2022-12-03 13:11:37 +0800

    new changes

--- app.js
+++ app.js
@@ -121,8 +121,35 @@ app.get("/v1/getAllTransactions" , async (req, res) => {
     });
 });
 
-//
 
+// Delete Scheduled Transaction
+
+app.post("/v1/delTransactions",  async (req, res) => {
+	//const { accountid, password, email, role } = req.body;
+	//const { accountid, password, email, role } = req.body;
+
+	const {TransactionID,AccountID,ReceivingAccountID,Date,TransactionAmount,Comment} = req.body;
+
+	//const hashedPassword = await bcrypt.hash(password, 10);
+
+
+ const delScheduledTransaction = new scheduledTransactions({
+	 TransactionID,
+	 AccountID,
+	 ReceivingAccountID,
+	 Date,
+	 TransactionAmount,
+	 Comment
+ });
+
+ delScheduledTransaction.save((err, result) => {
+	if (err) {
+		res.status(400).send({ message: "Error: Transaction already exists", error: err });
+	} else {
+		res.status(200).send({ message: "Transaction created successfully", data: "" });
+	}
+});
+});
 
 
 
