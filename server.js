fc7f144e04f6f2ad636350f38e82a3cbd5d8c46e
vanessageena <88197399+vanessageena@users.noreply.github.com>
2022-12-03 17:59:33 +0800

    Delete server.js

--- server.js
+++
@@ -1,6 +0,0 @@
-const app = require("./app");
-const { API_PORT } = process.env;
-const port = process.env.PORT || API_PORT;
-
-
-app.listen(port, () => console.log(`Application is listening on Port ${port}!`))
